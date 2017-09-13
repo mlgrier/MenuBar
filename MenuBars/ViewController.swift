d3ba3d8a7b9353000d5ba8c22146613014352d80
Unknown <mlgrier@gmail.com>
2017-09-12 22:01:36 -0500

    added a menu bar and some test code

--- MenuBars/ViewController.swift
+++ MenuBars/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func camTapped(_ sender: Any) {
+        print("Cam Pressed")
+    }
+    
 }
 
